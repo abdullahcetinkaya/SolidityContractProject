cf71e47e2b5e917404314cef46131550ff1619e1
Abdullah ÇETİNKAYA <abdullahcetinkayaa@gmail.com>
2023-12-24 17:50:43 +0300

    Update ProposalContract.sol

--- ProposalContract.sol
+++ ProposalContract.sol
@@ -12,14 +12,19 @@ contract ProposalContract {
         uint256 total_vote_to_end; // When the total votes in the proposal reaches this limit, proposal ends
         bool current_state; // This shows the current state of the proposal, meaning whether if passes of fails
         bool is_active; // This shows if others can vote to our contract
+        string title;
     }
 
 
     mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
+
+
+    function create(string calldata _description, uint256 _total_vote_to_end, string calldata _title) external {
+            counter += 1;
+            proposal_history[counter] = Proposal(_description, 0, 0, 0, _total_vote_to_end, false, true, _title);
+    }
+
 }
 
 
-function create(string calldata _description, uint256 _total_vote_to_end) external {
-        counter += 1;
-        proposal_history[counter] = Proposal(_description, 0, 0, 0, _total_vote_to_end, false, true);
-}
+
